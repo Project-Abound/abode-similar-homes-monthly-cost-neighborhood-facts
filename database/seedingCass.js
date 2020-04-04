8394a09f62a03146198448b739ab5c0b97ce8ace
Harrison Edwards <harrisonedwards@Harrisons-MacBook-Pro.local>
2020-04-04 11:24:06 -0700

    seeding script for cassandra updated. csv files added to gitignore. syntax errors fixed for schema

--- database/seedingCass.js
+++ database/seedingCass.js
@@ -34,57 +34,61 @@ const streetSuff = [
 
 // let neighborhoodId = () => { return Math.floor(Math.random() * Math.floor(14)) + 1 };
 
+for (let i = 1; i <= 1; i++) {
+  const csvWriter = createCsvWriter({
+    path: `csv/casshouses${i}.csv`,
+    header: [
+      {id: 'hood_id', title: 'hood_id'},
+      {id: 'hood_name', title: 'hood_name'},
+      {id: 'transit_score', title: 'transit_score'},
+      {id: 'walk_score', title: 'walk_score'},
+      {id: 'value_past', title: 'value_past'},
+      {id: 'value_future', title: 'value_future'},
+      {id: 'median_value', title: 'median_value'},
+      {id: 'house_id', title: 'house_id'},
+      {id: 'home_cost', title: 'home_cost'},
+      {id: 'bedrooms', title: 'bedrooms'},
+      {id: 'bathrooms', title: 'bathrooms'},
+      {id: 'square_feet', title: 'square_feet'},
+      {id: 'street_adr', title: 'street_adr'},
+      {id: 'city', title: 'city'},
+      {id: 'state', title: 'state'},
+      {id: 'zipcode', title: 'zipcode'},
+      {id: 'house_img', title: 'house_img'},
+    ]
+  });
 
+  const neighborhoods_houses = [];
 
-const csvWriter = createCsvWriter({
-  path: 'csv/cass1.csv',
-  header: [
-    {id: 'hood_id', title: 'hood_id'},
-    {id: 'hood_name', title: 'hood_name'},
-    {id: 'transit_score', title: 'transit_score'},
-    {id: 'walk_score', title: 'walk_score'},
-    {id: 'value_past', title: 'value_past'},
-    {id: 'value_future', title: 'value_future'},
-    {id: 'median_value', title: 'median_value'},
-    {id: 'house_id', title: 'house_id'},
-    {id: 'home_cost', title: 'home_cost'},
-    {id: 'bedrooms', title: 'bedrooms'},
-    {id: 'bathrooms', title: 'bathrooms'},
-    {id: 'square_feet', title: 'square_feet'},
-    {id: 'street_adr', title: 'street_adr'},
-    {id: 'city', title: 'city'},
-    {id: 'state', title: 'state'},
-    {id: 'zipcode', title: 'zipcode'},
-    {id: 'house_img', title: 'house_img'},
-  ]
-});
+  for (let n = 1; n <= 1000000; n++) {
+    let entry = {};
 
-const neighborhoods_houses = [];
+    entry.hood_id = faker.random.number({ min: 1, max: 15 });
+    entry.hood_name = sfHoods[entry.hood_id];
+    entry.transit_score = faker.random.number({ min: 70, max: 99 });
+    entry.walk_score = faker.random.number({ min: 70, max: 99 });
+    entry.value_past = faker.random.number({ min: -3, max: 4 });
+    entry.value_future = faker.random.number({ min: -3, max: 4 });
+    entry.median_value = faker.random.number({ min: 1100, max: 2200 }) * 1000;
+    entry.house_id = i;
+    entry.home_cost = Math.round((Math.floor(entry.median_value * faker.finance.amount(1.10, 1.30, 2))) / 1000) * 1000;
+    entry.bedrooms = faker.random.number({ min: 3, max: 6 });
+    entry.bathrooms = entry.bedrooms - faker.random.number({ min: 1, max: 2 });
+    entry.street_adr = faker.address.streetAddress();
+    entry.city = 'San Francisco';
+    entry.state = 'California';
+    entry.zipcode = faker.address.zipCode();
+    entry.house_img = faker.random.number({min: 1, max: 1000})
 
-const entryGenerator = () => {
-  let entry = {};
-  entry.hood_id = faker.random.number({ min: 1, max: 15 });
-  entry.hood_name = sfHoods[entry.hood_id];
-  entry.transit_score = faker.random.number({ min: 70, max: 99 });
-  entry.walk_score = faker.random.number({ min: 70, max: 99 });
-  entry.value_past = faker.random.number({ min: -3, max: 4 });
-  entry.value_future = faker.random.number({ min: -3, max: 4 });
-  entry.median_value = faker.random.number({ min: 1100, max: 2200 }) * 1000;
-  entry.bedrooms = faker.random.number({ min: 3, max: 6 });
-  entry.bathrooms = entry.bedrooms - faker.random.number({ min: 1, max: 2 });
-  entry.street_adr = faker.address.streetAddress();
-  entry.city = faker.address.city();
-  entry.state = faker.address.state();
-  entry.zipcode = faker.address.zipCode();
-  entry.house_img = faker.random.number({min: 1, max: 2})
-  return entry;
-}
+    neighborhoods_houses.push(entry)
+    if (n % 200000 === 0) {
+      console.log(`${n} completed of cass${i}.csv`);
+    }
+  }
 
-for (let i = 0; i < 1000000; i++) {
-  neighborhoods_houses.push(entryGenerator());
-}
-
-csvWriter.writeRecords(neighborhoods_houses)       // returns a promise
+  csvWriter.writeRecords(neighborhoods_houses)       // returns a promise
     .then(() => {
-        console.log('...Done');
+        console.log(`...Done with cass${i}.csv`);
     });
+
+};
