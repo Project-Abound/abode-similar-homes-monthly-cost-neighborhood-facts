9294adb617cbc02edcf3d6524e15a43b414964c6
Harrison Edwards <harrisonedwards@Harrisons-MacBook-Pro.local>
2020-04-06 10:37:21 -0700

    fixed schema, schema loaded into postgres no errors

--- database/pgschema.sql
+++ database/pgschema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE neighborhood_component;
 
-CREATE TABLE neighborhoods(
+CREATE TABLE neighborhoods_table(
   hood_id SERIAL PRIMARY KEY,
   hood_name VARCHAR (20) NOT NULL UNIQUE,
   transit_score INTEGER NOT NULL,
@@ -10,9 +10,9 @@ CREATE TABLE neighborhoods(
   median_value INTEGER NOT NULL
 );
  
-CREATE TABLE houses(
+CREATE TABLE houses_table(
   house_id SERIAL PRIMARY KEY,
-  hood_id INTEGER REFERENCES neighborhoods(hood_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  hood_id INTEGER REFERENCES neighborhood_table(hood_id) ON UPDATE CASCADE ON DELETE CASCADE,
   home_cost INTEGER NOT NULL,
   bedrooms INTEGER NOT NULL,
   bathrooms INTEGER NOT NULL,
@@ -24,12 +24,12 @@ CREATE TABLE houses(
   home_image VARCHAR (8)
 );
 
-CREATE TABLE users(
+CREATE TABLE users_table(
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(15)
 );
 
-CREATE TABLE user_houses(
-  user_id INTEGER REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  house_id INTEGER REFERENCES houses(house_id) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE user_houses_table(
+  user_id INTEGER REFERENCES user_table(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  house INTEGER REFERENCES house_table(house_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
