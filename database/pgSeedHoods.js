cda41f6f56330a1a02381e4198733fea8fe0e6d1
Harrison Edwards <harrisonedwards@Harrisons-MacBook-Pro.local>
2020-04-06 11:57:13 -0700

    seeding script for postgres neighborhoods table

---
+++ database/pgSeedHoods.js
@@ -0,0 +1,75 @@
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const faker = require('faker');
+
+const sfHoods = {
+  1: 'Hayes Valley',
+  2: 'Haight Ashbury',
+  3: 'Nob Hill',
+  4: 'North Beach',
+  5: 'SoMa',
+  6: 'The Mission',
+  7: 'Castro',
+  8: 'Outer Richmond',
+  9: 'Outer Sunset',
+  10: 'Pacific Heights',
+  11: 'Russian Hill',
+  12: 'Chinatown',
+  13: 'Buena Vista',
+  14: 'Fillmore',
+  15: 'Presidio'
+};
+
+const streetSuff = [
+  'St.',
+  'Rd.',
+  'Ave.',
+  'Ln.',
+  'Pl.',
+  'Ct.',
+  'Terr.',
+  'Blvd.',
+  'Wy.',
+  'Dr.',
+];
+
+// let neighborhoodId = () => { return Math.floor(Math.random() * Math.floor(14)) + 1 };
+
+for (let i = 1; i <= 10; i++) {
+  const csvWriter = createCsvWriter({
+    path: `csv/pg/pghoods${i}.csv`,
+    header: [
+      {id: 'hood_id', title: 'hood_id'},
+      {id: 'hood_name', title: 'hood_name'},
+      {id: 'transit_score', title: 'transit_score'},
+      {id: 'walk_score', title: 'walk_score'},
+      {id: 'value_past', title: 'value_past'},
+      {id: 'value_future', title: 'value_future'},
+      {id: 'median_value', title: 'median_value'},
+    ]
+  });
+
+  const neighborhoods = [];
+
+  for (let n = 1; n <= 1000000; n++) {
+    let entry = {};
+
+    entry.hood_id = faker.random.number({ min: 1, max: 15 });
+    entry.hood_name = sfHoods[entry.hood_id];
+    entry.transit_score = faker.random.number({ min: 70, max: 99 });
+    entry.walk_score = faker.random.number({ min: 70, max: 99 });
+    entry.value_past = faker.random.number({ min: -3, max: 4 });
+    entry.value_future = faker.random.number({ min: -3, max: 4 });
+    entry.median_value = faker.random.number({ min: 1100, max: 2200 }) * 1000;
+
+    neighborhoods.push(entry)
+    if (n % 200000 === 0) {
+      console.log(`${n} completed of pghoods${i}.csv`);
+    }
+  }
+
+  csvWriter.writeRecords(neighborhoods)       // returns a promise
+    .then(() => {
+        console.log(`...Done with pghoods${i}.csv`);
+    });
+
+};
--- database/pgschema.sql
+++ database/pgschema.sql
@@ -5,23 +5,23 @@ CREATE TABLE neighborhoods_table(
   hood_name VARCHAR (20) NOT NULL UNIQUE,
   transit_score INTEGER NOT NULL,
   walk_score INTEGER NOT NULL,
-  value_inc_dec_past INTEGER NOT NULL,
-  value_inc_dec_future INTEGER NOT NULL,
+  value_past INTEGER NOT NULL,
+  value_future INTEGER NOT NULL,
   median_value INTEGER NOT NULL
 );
  
 CREATE TABLE houses_table(
   house_id SERIAL PRIMARY KEY,
-  hood_id INTEGER REFERENCES neighborhood_table(hood_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  hood_id INTEGER REFERENCES neighborhoods_table(hood_id) ON UPDATE CASCADE ON DELETE CASCADE,
   home_cost INTEGER NOT NULL,
   bedrooms INTEGER NOT NULL,
   bathrooms INTEGER NOT NULL,
   street_address VARCHAR (30) NOT NULL,
   city VARCHAR(25),
-  us_state VARCHAR(13),
+  state VARCHAR(13),
   zipcode INTEGER,
-  squarefeet INTEGER,
-  home_image VARCHAR (8)
+  square_feet INTEGER,
+  house_img VARCHAR (8)
 );
 
 CREATE TABLE users_table(
@@ -30,6 +30,6 @@ CREATE TABLE users_table(
 );
 
 CREATE TABLE user_houses_table(
-  user_id INTEGER REFERENCES user_table(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  house INTEGER REFERENCES house_table(house_id) ON UPDATE CASCADE ON DELETE CASCADE
+  user_id INTEGER REFERENCES users_table(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  house INTEGER REFERENCES houses_table(house_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
